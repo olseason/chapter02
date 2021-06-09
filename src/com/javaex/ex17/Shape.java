9e5ca745f5d13fb0399a9c4c54bf9a059ef0c89c
olseason <sjlee2177@naver.com>
2021-06-09 10:33:10 +0900

    상속관계(도형, 사각형-원-삼각형)

--- src/com/javaex/ex17/Shape.java
+++ src/com/javaex/ex17/Shape.java
@@ -3,52 +3,50 @@ package com.javaex.ex17;
 public class Shape {
 
 	//필드
-	protected String fillColor;
-	protected String LineColor;
-	
-	
-	
-	//생성자
-	public Shape() {
+		protected String fillColor;
+		protected String lineColor;
 		
-	}
+		
+		//생성자
+		public Shape() {
+			//System.out.println("Shape()");
+		}
+		
+		public Shape(String fillColor, String lineColor) {
+			this.fillColor = fillColor;
+			this.lineColor = lineColor;
+			//System.out.println("Shape(2)");
+		}
 
-	public Shape(String fillColor, String LineColor) {
-		this.fillColor = fillColor;
-		this.LineColor =  LineColor;
-		System.out.println("shape(2)");
-	}
-	
-	
-	
-	
-	//메소드 g/s
-	public String getFillColor() {
-		return fillColor;
-	}
 
-	public void setFillColor(String fillColor) {
-		this.fillColor = fillColor;
-	}
+		//메소드-gs
+		public String getFillColor() {
+			return fillColor;
+		}
 
-	public String getLineColor() {
-		return LineColor;
-	}
+		public void setFillColor(String fillColor) {
+			this.fillColor = fillColor;
+		}
 
-	public void setLineColor(String lineColor) {
-		LineColor = lineColor;
-	}
-	
-	
-	//메소드 일반 
-	public String toString() {
-		return "Rectangle [fillColor=" + fillColor + ", LineColor=" + 
-				LineColor + "]";
-	}
-	
-	public void draw() {
-		System.out.println("[면색:" + fillColor + ", 선색:" + LineColor + "] 사각형을 그렸습니다.");
-	}
-	
+		public String getLineColor() {
+			return lineColor;
+		}
+
+		public void setLineColor(String lineColor) {
+			this.lineColor = lineColor;
+		}
+
+		//메소드-일반
+		@Override
+		public String toString() {
+			return "Shape [fillColor=" + fillColor + ", lineColor=" + lineColor + "]";
+		}
+		
+		public void draw() {
+			//[면색:빨강, 선색:검정] 도형을 그렸습니다.
+			System.out.println("[면색:" + fillColor + 
+					           ", 선색:" + lineColor + "] 도형을 그렸습니다.");
+		}
+		
 	
 }
